bdc74e9ea435e61c1b960e57f9dcdf17655d4637
SparkeEcrash <jpark121989@gmail.com>
2019-02-08 11:48:15 -0800

    add mysql setup

--- responsive_datatable.sql
+++ responsive_datatable.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2019 at 09:49 AM
+-- Generation Time: Feb 08, 2019 at 08:47 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -47,11 +47,12 @@ CREATE TABLE `responsive_datatable` (
 --
 
 INSERT INTO `responsive_datatable` (`id`, `first_name`, `last_name`, `telephone_number`, `street`, `city`, `state`, `zip_code`, `email_address`, `image`, `csv_export`) VALUES
-(1, 'jimmy', 'parker', '310-977-6534', '9200 Irvine Center Dr #200', 'Irvine', 'CA', '92618', 'jpark1219dev@gmail.com', 'iron_man.jpg', 0),
-(2, 'bruce', 'wayne', '973-331-3991', '3600 Wayne Tower Suite #10', 'Gotham', 'MD', '99999', 'batman@gmail.com', 'barack_obama.jpg', 0),
-(3, 'Donald', 'Trump', '931-323-4477', 'Pennsylvania Ave', 'Washington', 'DC', '20511', 'potus@gmail.com', 'yeonhee_lee.jpg', 1),
-(67, 'mickey', 'mouse', '333-333-3333', '124 Disney Drive', 'Miami', 'FL', '32444', 'test', '1549517148_ice_climbers_two.jpg', 1),
-(68, 'mickey', 'mouse', '333-333-3333', '124 Disney Drive', 'Miami', 'FL', '32444', 'test', '1549517315_ice_climbers_two.jpg', 1);
+(1, 'James', 'Bond', '310-977-6534', '9200 Irvine Center Dr #200', 'Irvine', 'CA', '92618', '007@mi6.com', '1549654104_james_bond.jpg', 1),
+(2, 'Bruce', 'Wayne', '973-331-3991', '3600 Wayne Tower Suite #10', 'Gotham', 'MD', '99999', 'batman@gmail.com', '1549654047_batman.jpg', 1),
+(3, 'Donald', 'Trump', '931-323-3567', 'Pennsylvania Ave', 'Washington', 'DC', '20511', 'potus@whitehouse.gov', '1549653951_donald_trump.jpg', 1),
+(67, 'Mickey', 'Mouse', '210-432-3492', '124 Disney Drive', 'Miami', 'FL', '32444', 'mickey@disney.com', '1549654205_mickey.jpg', 1),
+(72, 'Super', 'Mario', '983-274-2038', '38 Nintendo Court', 'Boston', 'MA', '32444', 'mario@nintendo.com', '1549654233_mario.jpg', 1),
+(73, 'James', 'Park', '332-754-5514', '8 Waterford Drive', 'Metropolis', 'WA', '34522', 'jpark@gmail.com', '1549653891_Profile Picture.jpg', 1);
 
 --
 -- Indexes for dumped tables
@@ -71,9 +72,8 @@ ALTER TABLE `responsive_datatable`
 -- AUTO_INCREMENT for table `responsive_datatable`
 --
 ALTER TABLE `responsive_datatable`
-  MODIFY `id` mediumint(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` mediumint(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
